d66afe33783452c9f4ab55d70292e69cdacecbba
Achmad Fatah Ibrahim <acfatah@gmail.com>
2024-04-12 22:13:28 +0800

    Update npm scripts

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,12 +3,18 @@ require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
   root: true,
-  'extends': [
+  extends: [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
     '@vue/eslint-config-prettier/skip-formatting'
   ],
   parserOptions: {
     ecmaVersion: 'latest'
-  }
+  },
+  ignorePatterns: ['package*.json', '.gitignore', '.simple-git-hooks.json', 'README.md'],
+  overrides: [
+    {
+      files: ['.vue', '.js', '.jsx', '.cjs', '.mjs']
+    }
+  ]
 }
